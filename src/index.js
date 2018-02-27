ff92be13827669c36f32d625117cc788268106ce
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-27 23:07:17 +0300

    task 04

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+  var arrEquation = [];
+  var a = arr[0];
+  var b = arr[4];
+  var c = arr[8];
+  if(arr[3] === "-"){
+    b = -b;
+  }
+  if(arr[7] === "-"){
+    c = -c;
+  }
+  var d = b * b - 4 * a * c;
+  var x1 = Math.round(-b + Math.sqrt(d)) / (2 * a);
+  var x2 = Math.round(-b - Math.sqrt(d)) / (2 * a);
+  arrEquation.push(x1);
+  arrEquation.push(x2);
+  if (x1 > x2) {
+    arrEquation = [x2, x1];
+  }
+  else {
+    arrEquation = [x1, x2];
+  }
+  return arrEquation;
+  }
 }
