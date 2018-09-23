791eca78b8b0864928bca8007173cc3048783ec6
IharTsykala <Sudja09@bk.ru>
2018-09-24 02:31:57 +0300

    2018q3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
+
 module.exports = function solveEquation(equation) {
-  var arr = equation.split(' ');
-  var arrEquation = [];
-  var a = arr[0];
-  var b = arr[4];
-  var c = arr[8];
-  if(arr[3] === "-"){
-    b = -b;
-  }
-  if(arr[7] === "-"){
-    c = -c;
-  }
-  var d = b * b - 4 * a * c;
-  var x1 = Math.round(-b + Math.sqrt(d)) / (2 * a);
-  var x2 = Math.round(-b - Math.sqrt(d)) / (2 * a);
-  arrEquation.push(x1);
-  arrEquation.push(x2);
-  if (x1 > x2) {
-    arrEquation = [x2, x1];
-  }
-  else {
-    arrEquation = [x1, x2];
-  }
-  return arrEquation;
-}
+  
+    let arrToEnter = equation.split(" ");
+    let arrSort = [];
+    let a = arrToEnter[0];
+    let b = arrToEnter[4];
+    let c = arrToEnter[8];
+        
+    if (arrToEnter[3] === "-") b = -b;
+    if (arrToEnter[7] === "-") c = -c;
+        
+    let d = (b*b)-(4*a*c);           
+        
+    let x1 = Math.round(-b + Math.sqrt(d))/(2*a);
+    let x2 = Math.round(-b - Math.sqrt(d))/(2*a);
+    arrSort.push(x1);
+    arrSort.push(x2);
+
+    function compareNumeric(a, b) {
+        return a - b;
+    }            
+      
+    return arrSort.sort(compareNumeric);        
+};
+    
+    
+
